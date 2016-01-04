ProjectGero/platform_cts

Remove a cross-profile app linking test.
Remove the case where:
- there is a receiver with status always in the primary.
- there is no receiver with status always in the work profile.

BUG:25109215
Change-Id: I5328775a7439ee3768b81e8110edd3dae84c606c

hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/ManagedProfileTest.java
@@ -205,13 +205,9 @@ public class ManagedProfileTest extends BaseDevicePolicyTest {
         // Now there's only the browser in the managed profile left
         assertAppLinkResult("testReceivedByBrowserActivityInManaged");
 
-        changeVerificationStatus(USER_OWNER, INTENT_RECEIVER_PKG, "always");
-        changeVerificationStatus(mUserId, INTENT_RECEIVER_PKG, "ask");
-        // We've set the receiver in the primary user to always: only this one should receive the
-        // intent.
-        assertAppLinkResult("testReceivedByAppLinkActivityInPrimary");
 
         changeVerificationStatus(mUserId, INTENT_RECEIVER_PKG, "always");
+        changeVerificationStatus(USER_OWNER, INTENT_RECEIVER_PKG, "always");
         // We have one always in the primary user and one always in the managed profile: the managed
         // profile one should have precedence.
         assertAppLinkResult("testReceivedByAppLinkActivityInManaged");

vchong/cts

BUG:26312621

hostsidetests/devicepolicy/src/com/android/cts/devicepolicy/ManagedProfileTest.java
@@ -210,13 +210,8 @@ public class ManagedProfileTest extends BaseDevicePolicyTest {
         // Now there's only the browser in the managed profile left
         assertAppLinkResult("testReceivedByBrowserActivityInManaged");
 
-        changeVerificationStatus(mParentUserId, INTENT_RECEIVER_PKG, "always");
-        changeVerificationStatus(mProfileUserId, INTENT_RECEIVER_PKG, "ask");
-        // We've set the receiver in the primary user to always: only this one should receive the
-        // intent.
-        assertAppLinkResult("testReceivedByAppLinkActivityInPrimary");
-
         changeVerificationStatus(mProfileUserId, INTENT_RECEIVER_PKG, "always");
+        changeVerificationStatus(mParentUserId, INTENT_RECEIVER_PKG, "always");
         // We have one always in the primary user and one always in the managed profile: the managed
         // profile one should have precedence.
         assertAppLinkResult("testReceivedByAppLinkActivityInManaged");